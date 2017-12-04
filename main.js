43c1edd0bb4bf55c25dfad2f6c249fdaa145139d
Thiago M. Medeiros <thiagomunhozmedeiros@gmail.com>
2017-12-04 17:25:44 -0200

    chore: updates the keycode that need to be pressed
    - now the key is Ctrl + q
    - remore unecessary folder structure

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 let firstKey = false
 
 window.onkeydown = function (e) {
-  if (e.keyCode === 84) {
+  if (e.keyCode === 17) {
     firstKey = true
     setTimeout(() => {
       firstKey = false
     }, 1000)
   }
 
-  if (firstKey && e.keyCode === 187) {
+  if (firstKey && e.keyCode === 81) {
     const selectedText = window.getSelection().toString()
     window.open('https://translate.google.com.br/#en/pt/'+selectedText)
   }
